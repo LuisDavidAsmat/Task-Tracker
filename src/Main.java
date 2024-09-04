0f2af66d9a7cb64fc893e35866b66f32e1f29d97
david <david.exe2@gmail.com>
2024-09-03 20:08:48 -0500

    delete unnecessary commented lines

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,5 @@ public class Main
         TaskManager taskManager = new TaskManager(taskFileHandler);
         TaskCLI taskCLI = new TaskCLI(taskManager);
         taskCLI.handleUserCommand(args);
-        // Example: Add a task
-        //taskManager.addTask("Walk the dog");
-       //taskManager.addTask("Write the report");
-        //taskManager.addTask("Descansar no teu peito");
-        //taskManager.updateTaskDescription(2, "Here it comes");
-        //taskManager.updateTaskStatus("in-progress", 2);
-        //taskManager.updateTaskStatus("in-progress", 1);
-        //taskManager.deleteTask(2);
-
-        // Load and display tasks
-        //taskManager.printAllTasks();
-        //taskManager.printTasksByStatus("done");
-        //taskManager.printTasksByStatus("in-progress");
     }
 }
